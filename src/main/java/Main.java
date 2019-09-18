e9bc17e846e94f8ad3255ad2054bff5edc12e7fd
Luiz Guilherme Hauth <luiz.g.hauth@bradesco.com.br>
2019-09-18 15:41:11 -0300

    - First Step

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     static String checkStatus(int grade) {
-        if (grade > 0) {
+        if (grade > 7) {
             return "Approved".toUpperCase();
+        } else {
+            return "Not Approved".toUpperCase();
         }
-        return null;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -11,4 +11,14 @@ class MainTest {
         //Then
         Assert.assertEquals("Approved".toUpperCase(), status);
     }
+
+    @Test
+    void given_a_grade_lower_than_seven_then_status_not_approved() {
+        //Given
+        int grade = 3;
+        //When
+        String status = Main.checkStatus(grade);
+        //Then
+        Assert.assertEquals("Not Approved".toUpperCase(), status);
+    }
 }
