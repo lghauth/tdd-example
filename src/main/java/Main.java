a12e8ebebcd77db6c46166fae086e3fed2a7fc63
Luiz Guilherme Hauth <luiz.g.hauth@bradesco.com.br>
2019-09-19 15:22:09 -0300

    - Fifth Step

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,30 @@
 public class Main {
+    public static void main(String args[]) {
+        System.out.println(checkStatus(7).toUpperCase());
+        System.out.println(checkStatus(10).toUpperCase());
+
+        System.out.println(checkStatus(0).toUpperCase());
+        System.out.println(checkStatus(6).toUpperCase());
+
+        System.out.println(checkStatus(-1).toUpperCase());
+        System.out.println(checkStatus(11).toUpperCase());
+    }
+
     static String checkStatus(int grade) {
-        String status = "";
+        if (isApproved(grade))
+            return "Approved".toUpperCase();
 
-        if (grade >= 0 && grade <= 10) {
-            if (grade >= 7 && grade <= 10) {
-                status = "Approved".toUpperCase();
-            }
+        if (isNotApproved(grade))
+            return "Not Approved".toUpperCase();
 
-            if (grade >= 0 && grade <= 6) {
-                status = "Not Approved".toUpperCase();
-            }
-        } else {
-            status = "Error".toUpperCase();
-        }
+        return "Error".toUpperCase();
+    }
 
-        return status;
+    private static boolean isNotApproved(int grade) {
+        return grade >= 0 && grade <= 6;
+    }
+
+    private static boolean isApproved(int grade) {
+        return grade >= 7 && grade <= 10;
     }
 }
